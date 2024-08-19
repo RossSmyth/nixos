83f710df6d35048d7b5fd93aee90ea23c022115c
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2024-08-19 14:38:20 -0400

    Add Bogsher theme

--- home-manager/helix.nix
+++ home-manager/helix.nix
@@ -8,7 +8,7 @@
     enable = true;
     defaultEditor = true;
     settings = {
-      theme = "bogster";
+      theme = "bogsher";
 
       editor = {
         true-color = true;
@@ -23,6 +23,20 @@
         lsp.display-messages = true;
         file-picker.hidden = false;
         file-picker.git-ignore = true;
+      };
+    };
+    themes.bogsher = {
+      inherits = "bogster";
+      "variable" = "bogster-fg1";
+      "variable.other.member" = "bogster-fg1";
+      "variable.parameter" = "bogster-fg1";
+      "identifier" = "bogster-fg1";
+      "ui.virtual.inlay-hint" = "bogster-fg0";
+      "ui.selection.bg" = "bogster-base3";
+      "ui.selection.primary.bg" = "bogster-base4";
+      "ui.virtual.jump-label" = {
+        bg = "bogster-base3";
+        modifiers = ["bold"];
       };
     };
   };
