dfbebc02188558104f155f20699127f86c1b5ecc
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2024-08-19 13:07:40 -0400

    Try #2

--- flake.nix
+++ flake.nix
@@ -2,9 +2,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    helix.url = "github:helix-editor/helix";
   };
 
-  outputs = { self, nixpkgs, nixos-wsl, ... }: {
+  outputs = { self, nixpkgs, nixos-wsl, home-manager, ... } @ inputs: let
+    inherit (self) outputs;
+  in {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -18,5 +26,12 @@
         ];
       };
     };
+  homeConfigurations = {
+    "rsmyth@nixos" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      extraSpecialArgs = {inherit inputs outputs;};
+      modules = [./home-manager/home.nix];
+    };
+  };
   };
 }
