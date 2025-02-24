72a9452d52850ffea581dbbdc805f4a2e0116f67
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-02-24 12:14:14 -0500

    Update git config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -57,13 +57,35 @@
         cm = "commit -m";
       };
       extraConfig = {
-        push.default = "current";
-        push.autoSetupRemote = true;
+        push = {
+          default = "current";
+          followTags = true;
+          autoSetupRemote = true;
+        };
+        fetch = {
+          prune = true;
+          pruneTags = true;
+          all = true;
+        };
+        rebase = {
+          autoSquash = true;
+          autoStash = true;
+          updateRefs = true;
+        };
         core.autocrlf = false;
-        pull.ff = "only";
+        pull = {
+          ff = "only";
+          rebase = true;
+        };
         init.defaultBranch = "main";
         merge.conflictstyle = "zdiff3";
         blame.ignoreRevsFile = ".git-blame-ignore-revs";
+        help.autocorrect = "prompt";
+        commit.verbose = true;
+        rerere = {
+          enabled = true;
+          autoupdate = true;
+        };
       };
     };
 
