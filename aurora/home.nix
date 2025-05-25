3ed71e7dcff8e0670ad70cf5adcd14b4d8528631
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-05-25 18:17:50 -0400

    Attempt to fix Sway

--- aurora/home.nix
+++ aurora/home.nix
@@ -5,11 +5,16 @@
   ...
 }:
 {
+  services.gnome-keyring.enable = true;
   wayland.windowManager.sway = {
     enable = true;
     checkConfig = true;
+    wrapperFeatures.gtk = true;
     config = {
       terminal = "${lib.getExe pkgs.alacritty}";
+      startup = [
+        { command = "alacritty"; }
+      ];
     };
   };
 
