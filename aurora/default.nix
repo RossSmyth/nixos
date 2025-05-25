d2e22136af91b352795fcbcb84c7ff0a31f718d0
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-05-25 18:17:50 -0400

    Add Nvidia Optimus Prime config

--- aurora/default.nix
+++ aurora/default.nix
@@ -33,10 +33,14 @@
   hardware.graphics.enable = true;
   hardware.nvidia = {
     modesetting.enable = true;
-
     powerManagement.finegrained = true;
-
     open = true;
+
+    # For Optimus PRIME hybrid graphics
+    prime = {
+      intelBusId = "00:02.0";
+      nvidiaBusId = "01:00.0";
+    };
 
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
