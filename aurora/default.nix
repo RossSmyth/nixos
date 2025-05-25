b7e928bd7f04e3d8402f277341ac062ae248f35b
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-05-25 18:17:50 -0400

    Try enabling Nvidia drivers

--- aurora/default.nix
+++ aurora/default.nix
@@ -2,18 +2,23 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Enable sound.
   services.pulseaudio.enable = true;
@@ -23,8 +28,19 @@
 
   # Half-decent TTY font
   console.font = "${pkgs.terminus_font}/share/consolefonts/ter-u28n.pdf.gz";
-  
+
+  # GPU Stuff
+  hardware.graphics.enable = true;
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    powerManagement.finegrained = true;
+
+    open = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
   system.stateVersion = "24.05"; # Did you read the comment?
 
 }
-
