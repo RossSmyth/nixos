a799482b5a8693a8e7b4435050d8a5953a696bf7
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-05-25 18:17:50 -0400

    try adding to more groups

--- default.nix
+++ default.nix
@@ -64,7 +64,7 @@
       useDefaultShell = true;
       hashedPassword = "$y$j9T$pANX.P1IbyQB2xriv3ncp/$AnA0t/0WrMitJYBivHKlcdp0d8lqbCuR0yN1zvOnDFA";
       isNormalUser = true;
-      extraGroups = [ "wheel" ];
+      extraGroups = [ "wheel" "input" "networkmanager" "audio" "docker" ];
     };
   };
 
