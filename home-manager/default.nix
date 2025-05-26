8e4719077343c0aafc8c5c4ce16a4a99407e82d9
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-05-26 13:08:08 -0400

    Remove silent nom patch
    Ok actually I like the verbose output :3

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -16,13 +16,7 @@
     username = "rsmyth";
     homeDirectory = "/home/rsmyth";
     packages = [
-      # Add NOM but disable the build logs
-      # https://github.com/maralorn/nix-output-monitor/issues/59
-      (pkgs.nix-output-monitor.overrideAttrs (
-        final: prev: {
-          patches = (prev.patches or [ ]) ++ [ ./silent-nom.patch ];
-        }
-      ))
+      pkgs.nix-output-monitor
     ];
   };
 
