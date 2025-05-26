d1aa19678e3a01108eddf4015b7fcbb3b285eda7
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-05-26 13:20:21 -0400

    Reformat fonts

--- fonts.nix
+++ fonts.nix
@@ -6,17 +6,21 @@
   console.font = "Lat2-Terminus16";
 
   # System-wide fonts
-  fonts.packages = with pkgs; [
-    nerd-fonts.inconsolata
-    nerd-fonts.noto
-    noto-fonts-emoji
-  ];
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.inconsolata
+      nerd-fonts.noto
+      noto-fonts-emoji
+    ];
 
-  # Font defaults
-  fonts.fontconfig.enable = true;
-  fonts.fontconfig.defaultFonts = {
-    serif = [ "Noto Serif" ];
-    sansSerif = [ "Noto Sans" ];
-    monospace = [ "Inconsolata Nerd Font Mono" ];
+    # Font defaults
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        serif = [ "Noto Serif" ];
+        sansSerif = [ "Noto Sans" ];
+        monospace = [ "Inconsolata Nerd Font Mono" ];
+      };
+    };
   };
 }
