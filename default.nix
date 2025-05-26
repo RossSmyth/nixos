bdae15de1ed2379d6f15c766fe43da921a0fe3f6
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-05-26 13:05:20 -0400

    coreutils -> uutils

--- default.nix
+++ default.nix
@@ -75,5 +75,11 @@
     fsType = "tmpfs";
   };
 
+  # Replace coreutils with uutils
+  # Need hiPrio or else there are collisions
+  environment.systemPackages = with pkgs; [
+    (lib.hiPrio uutils-coreutils-noprefix)
+  ];
+
   system.stateVersion = "24.05";
 }
