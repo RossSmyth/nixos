cbea872a9f99009653f836b43c83eb210cb71ecb
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-06-03 16:24:42 -0400

    Disable editing kernel vars, enable memtest

--- nixos/bootloader.nix
+++ nixos/bootloader.nix
@@ -1,7 +1,11 @@
 { user, ... }:
 {
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    memtest86.enable = true;
+    editor = false;
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Login automatically on the TTY on start
