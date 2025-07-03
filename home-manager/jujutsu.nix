abbd549622014d0e52e50c7e51b39a5e84e73590
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-07-02 22:58:57 -0400

    Use git verbose format in jj

--- home-manager/jujutsu.nix
+++ home-manager/jujutsu.nix
@@ -61,6 +61,24 @@
         write-change-id-header = true;
       };
       snapshot.auto-update-stale = true;
+      templates = {
+        draft_commit_description = ''
+            concat(
+              description,
+              surround(
+                "\nJJ: This commit contains the following changes:\n", "",
+                indent("JJ:     ", diff.summary()),
+              ),
+              surround(
+                "\nJJ: This commit contains the following changes:\n", "",
+                indent("JJ:     ", diff.stat(72)),
+              ),
+              "\n",
+              "JJ: ignore-rest\n",
+              diff.git(),
+            )
+          '';
+      };
     };
   };
 }
