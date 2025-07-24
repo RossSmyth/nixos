e18a33bd17413ca61bd728d1f4b8df19ccfe99f3
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-07-24 10:53:22 -0400

    Update aliases

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -14,13 +14,15 @@
       end
     '';
     shellAliases = {
-      cat = "${lib.getExe pkgs.bat} --paging=never";
-      nxswitch = "sudo nixos-rebuild switch --flake ${config.xdg.configHome}/nix";
-      nxbuild = "sudo nixos-rebuild boot --flake ${config.xdg.configHome}/nix";
-      nxedit = "${config.home.sessionVariables.EDITOR} ${config.xdg.configHome}/nix";
-      getLargest = "${lib.getExe pkgs.fd} -t file . --exec ls -s | sort -nr | head -n20";
-      dev = "nix develop --command ${lib.getExe pkgs.fish}";
-      scratch = ''systemd-run --property=PrivateTmp=true --description "scratch shell" --user --collect --shell --working-dir "/var/tmp"'';
+      update = ''${lib.getExe pkgs.nix} flake update -L --show-trace --log-format internal-json &| ${lib.getExe pkgs.nom} --json'';
+      nbuild = ''${lib.getExe pkgs.nixos-rebuild-ng} boot --log-format internal-json --show-trace --flake ${config.xdg.configHome}# &| ${lib.getExe pkgs.nom} --json'';
     };
+  };
+  home.shellAliases = {
+    cat = "${lib.getExe pkgs.bat} --paging=never";
+    nxedit = "${config.home.sessionVariables.EDITOR} ${config.xdg.configHome}/nix";
+    getLargest = "${lib.getExe pkgs.fd} -t file . --exec ls -s | sort -nr | head -n20";
+    dev = "nix develop --command ${config.home.sessionVariables.EDITOR}";
+    scratch = ''systemd-run --property=PrivateTmp=true --description "scratch shell" --user --collect --shell --working-dir "/var/tmp"'';
   };
 }
