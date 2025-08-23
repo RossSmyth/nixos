893c55d97dd26f06f7f90f111ec5a3a78de19b4d
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-08-23 17:27:41 -0400

    uutils: :3/Linux

--- nixos/coreutils.nix
+++ nixos/coreutils.nix
@@ -1,8 +1,30 @@
 { pkgs, lib, ... }:
+let
+  latestNightly = pkgs.rust-bin.nightly.latest.minimal;
+  rustPlatform = pkgs.makeRustPlatform {
+    rustc = latestNightly;
+    cargo = latestNightly;
+    stdenv = pkgs.clangStdenv;
+  };
+  uutils = (pkgs.uutils-coreutils-noprefix.override {
+    inherit rustPlatform;
+  }).overrideAttrs {
+    postPatch = ''
+        substituteInPlace ./src/uu/uname/src/uname.rs \
+          --replace-fail '(opts.os || opts.all).then(|| uname.osname().to_string_lossy().to_string())' 'Some(":3/Linux".to_string())'
+      '';
+    env.RUSTFLAGS = "-Ctarget-cpu=native";
+    cargoBuildFlags = [
+      "--config" "profile.release.lto=thin"
+      "--config" "profile.release.panic=abort"
+    ];
+    doCheck = false;
+  };
+in
 {
   # Replace coreutils with uutils
   # Need hiPrio or else there are collisions
-  environment.systemPackages = with pkgs; [
-    (lib.hiPrio uutils-coreutils-noprefix)
+  environment.systemPackages = [
+    (lib.hiPrio uutils)
   ];
 }
