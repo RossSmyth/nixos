432d5c97a77f60f5cc9f05d2004b4789cee15787
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-10-05 18:17:05 -0400

    nixos: Disable memtest
    I've never used it

--- nixos/bootloader.nix
+++ nixos/bootloader.nix
@@ -3,7 +3,6 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot = {
     enable = true;
-    memtest86.enable = true;
     editor = false;
   };
   boot.loader.efi.canTouchEfiVariables = true;
