b8b40f386aa26bc79b3528b1fd8081fcfca7c67b
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-10-05 23:48:37 -0400

    flake: Add helix substituter

--- flake.nix
+++ flake.nix
@@ -1,4 +1,8 @@
 {
+  nixConfig = {
+    extra-substituters = ["https://helix.cachix.org"];
+    extra-trusted-public-keys = ["helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="];
+  };
   inputs = {
     nixpkgs.url = "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz";
 
