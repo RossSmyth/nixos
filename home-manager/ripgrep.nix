d589ac34d13af551df2c72540f38dd6d562e26ac
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-10-05 23:00:47 -0400

    hm: Use Nixpkgs rust for source rg

--- home-manager/ripgrep.nix
+++ home-manager/ripgrep.nix
@@ -1,25 +1,25 @@
-{ pkgs, inputs, ... }:
+{
+  lib,
+  pkgs,
+  inputs,
+  fromSource,
+  ...
+}:
 let
-  latestNightly = pkgs.rust-bin.nightly.latest.minimal;
-  rustPlatform = pkgs.makeRustPlatform {
-    rustc = latestNightly;
-    cargo = latestNightly;
-    stdenv = pkgs.clangStdenv;
-  };
-  package = (pkgs.ripgrep.override { inherit rustPlatform; }).overrideAttrs {
+  # TODO: Remove at next ripgrep release
+  package = pkgs.ripgrep.overrideAttrs {
     src = inputs.ripgrep;
-    cargoDeps = rustPlatform.importCargoLock {
+    cargoDeps = pkgs.rustPlatform.importCargoLock {
       lockFile = "${inputs.ripgrep}/Cargo.lock";
       allowBuiltinFetchGit = true;
     };
     cargoHash = null;
-    env.RUSTFLAGS = "-Ctarget-cpu=native -Cpanic=abort -Clto=thin -Cembed-bitcode=yes";
     doCheck = false;
   };
 in
 {
   programs.ripgrep = {
-    inherit package;
+    package = lib.mkIf fromSource package;
     enable = true;
   };
 }
