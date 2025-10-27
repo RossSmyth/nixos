44dc86ba7173e8e8c394e9027f4bf6ceda4bcb64
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-10-26 22:43:38 -0400

    hm: No confirm ghostty close surface

--- home-manager/ghostty.nix
+++ home-manager/ghostty.nix
@@ -5,6 +5,7 @@
     settings = {
       linux-cgroup = "always";
       linux-cgroup-hard-fail = true;
+      confirm-close-surface = false;
       font-size = 13;
       font-family = "Inconsolata Nerd Font Mono";
       font-family-bold = "Inconsolata Nerd Font Mono";
