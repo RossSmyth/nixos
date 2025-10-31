66ff8e823fafc295925605a428b7518db1b2d921
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-10-31 18:03:38 -0400

    nixos: Actually enable Nvidia drivers

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -2,6 +2,7 @@
 {
   # GPU Stuff
   hardware.graphics.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     open = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
