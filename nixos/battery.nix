abc625bb7b1a007790432b8586f5a06dfe91e4db
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-12-12 15:18:32 -0500

    nixos: Once powertop is enabled, disable autosuspend for all HID devices

--- nixos/battery.nix
+++ nixos/battery.nix
@@ -1,8 +1,21 @@
+{ lib, config, ... }:
 {
   # For laptops attempting to save battery life
+
+  # Rule to disable all auto-suspend rules for HID devices
+  services.udev.extraRules = ''
+    ACTION=="add|bind", SUBSYSTEM=="input", TEST=="power/control", ATTR{power/control}="on"
+  '';
+
   powerManagement = {
     enable = true;
-    powertop.enable = true;
+    powertop = {
+      enable = true;
+      # Ensure HID devices are not suspended once powertop starts
+      postStart = ''
+        ${lib.getExe' config.systemd.package "udevadm"} trigger --action=bind --subsystem-match=input
+      '';
+    };
     cpuFreqGovernor = "powersave";
   };
   services = {
