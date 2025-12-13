32c93821d9b238d55027f370460fa647589840e9
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-12-13 14:39:30 -0500

    nixos: Make battery a specialization

--- nixos/battery.nix
+++ nixos/battery.nix
@@ -1,36 +1,25 @@
 { lib, config, ... }:
 {
-  # For laptops attempting to save battery life
-
-  # Rule to disable all auto-suspend rules for HID devices
-  services.udev.extraRules = ''
-    ACTION=="add|bind", SUBSYSTEM=="input", TEST=="power/control", ATTR{power/control}="on"
-  '';
-
-  powerManagement = {
-    enable = true;
-    powertop = {
+  specialisation.battery.configuration = {
+    powerManagement = {
       enable = true;
-      # Ensure HID devices are not suspended once powertop starts
-      postStart = ''
-        ${lib.getExe' config.systemd.package "udevadm"} trigger --action=bind --subsystem-match=input
-      '';
+      powertop.enable = true;
+      cpuFreqGovernor = "powersave";
     };
-    cpuFreqGovernor = "powersave";
-  };
-  services = {
-    thermald.enable = true;
-    power-profiles-daemon.enable = false;
+    services = {
+      thermald.enable = true;
+      power-profiles-daemon.enable = false;
 
-    auto-cpufreq = {
-      enable = true;
-      settings = {
-        battery = {
-          governor = "powersave";
-          turbo = "never";
-        };
-        charger = {
-          turbo = "auto";
+      auto-cpufreq = {
+        enable = true;
+        settings = {
+          battery = {
+            governor = "powersave";
+            turbo = "never";
+          };
+          charger = {
+            turbo = "auto";
+          };
         };
       };
     };
