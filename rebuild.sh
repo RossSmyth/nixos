2fd592637102758e0f0f9a729b661a16b7a6f16e
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-03-11 22:09:03 -0400

    use nom

--- rebuild.sh
+++ rebuild.sh
@@ -6,5 +6,5 @@ shift
 
 nixpkgs="$(, npins get-path nixpkgs)"
 
-NIX_PATH="nixpkgs=$nixpkgs" nixos-rebuild "$cmd" --attr "$(hostname)" "$@"
+NIX_PATH="nixpkgs=$nixpkgs" pkexec --keep-cwd nixos-rebuild "$cmd" --log-format internal-json --show-trace --attr "$(hostname)" "$@" |& nom --json
 popd
