18b93b480551fcdc252749e757bbfef909f4bb58
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-03-31 17:18:48 -0400

    rebuild.sh: Actually set NIX_PATH

--- rebuild.sh
+++ rebuild.sh
@@ -4,7 +4,7 @@ pushd "$(dirname $0)"
 cmd="${1:-"dry-build"}"
 shift
 
-nixpkgs="$(, npins get-path nixpkgs)"
+nixpkgs="$(npins get-path nixpkgs)"
 
-NIX_PATH="nixpkgs=$nixpkgs" pkexec --keep-cwd nixos-rebuild "$cmd" --log-format internal-json --show-trace --attr "$(hostname)" "$@" |& nom --json
+pkexec --keep-cwd nixos-rebuild "$cmd" -I nixpkgs="$nixpkgs" --log-format internal-json --show-trace --attr "$(hostname)" "$@" |& nom --json
 popd
