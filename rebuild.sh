7ccff013bafe7218afc16ff2439ded1b2b1a1b5d
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-05-25 16:23:09 -0400

    rebuild.sh: use run0

--- rebuild.sh
+++ rebuild.sh
@@ -6,5 +6,5 @@ shift
 
 nixpkgs="$(npins get-path nixpkgs)"
 
-pkexec --keep-cwd nixos-rebuild "$cmd" -I nixpkgs="$nixpkgs" --log-format internal-json --show-trace --attr "$(hostname)" "$@" |& nom --json
+run0 nixos-rebuild "$cmd" -I nixpkgs="$nixpkgs" --log-format internal-json --show-trace --attr "$(hostname)" "$@" |& nom --json
 popd
