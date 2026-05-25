5b6b5bdf5c24f7bfc5cd5ecc13c9298500fdb6cf
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-05-25 14:31:43 -0400

    nixos: put media in a global folder

--- nixos/torrent.nix
+++ nixos/torrent.nix
@@ -6,7 +6,12 @@
     openRPCPort = true;
     openPeerPorts = true;
 
+    # Downloads to a jellyfin-controlled dir
+    downloadDirPermissions = null;
+
     settings = {
+      # Find a way to automate sorting TV and movies
+      download-dir = "/media";
       rpc-bind-address = "0.0.0.0";
       rpc-whitelist = "127.0.0.1,192.168.*.*";
     };
