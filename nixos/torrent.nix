a35b62d2dcc1ec181a4778f8b18580d1e3ac497d
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-05-25 00:20:00 -0400

    nixos: make the rpc actually work, web client still does not

--- nixos/torrent.nix
+++ nixos/torrent.nix
@@ -2,5 +2,11 @@
   services.transmission = {
     enable = true;
     openRPCPort = true;
+    openPeerPorts = true;
+
+    settings = {
+      rpc-bind-address = "0.0.0.0";
+      rpc-whitelist = "127.0.0.1,192.168.*.*";
+    };
   };
 }
