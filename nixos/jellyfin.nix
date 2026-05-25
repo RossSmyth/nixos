26fbd0914f966d32cd1a1686b880a15561cd145a
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-05-25 16:22:37 -0400

    nixos: Add reverse proxy on top of jellyfin for exposure

--- nixos/jellyfin.nix
+++ nixos/jellyfin.nix
@@ -3,4 +3,13 @@
     enable = true;
     openFirewall = true;
   };
+
+  # Expose to the world
+  services.caddy = {
+    enable = true;
+
+    virtualHosts."jellyfin.rsmyth.net".extraConfig = ''
+      reverse_proxy :8096
+    '';
+  };
 }
