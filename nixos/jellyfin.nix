7dd74671f714573e988318c1cde9cb9226d80b21
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-05-26 17:06:28 -0400

    nixos/jellyfin: logging to stdout for journald

--- nixos/jellyfin.nix
+++ nixos/jellyfin.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   services.jellyfin = {
+    # machine specific hardware options
     enable = true;
     openFirewall = true;
   };
@@ -10,24 +11,39 @@
     enable = true;
 
     package = pkgs.caddy.withPlugins {
+      # Need this for some reason.
       plugins = [
         "github.com/caddy-dns/cloudflare@v0.2.4"
       ];
       hash = "sha256-bzMqxWTqrJ1skZmRTXyEMCKStXpljbqe5r0Ve2cnBfM=";
     };
 
+    # Just log for journald
+    logFormat = ''
+      output stdout
+      format console
+    '';
+
+    # For accessing internally on my LAN
     virtualHosts."192.168.1.11".extraConfig = ''
       reverse_proxy :8096
       tls internal
     '';
 
-    virtualHosts."jellyfin.rsmyth.net".extraConfig = ''
-      tls {
-        dns cloudflare {env.CF_API_TOKEN}
-        resolvers 1.1.1.1
-      }  
-      reverse_proxy :8096
-    '';
+    # For external users
+    virtualHosts."jellyfin.rsmyth.net" = {
+      logFormat = ''
+        output stdout
+        format console
+      '';
+      extraConfig = ''
+        tls {
+          dns cloudflare {env.CF_API_TOKEN}
+          resolvers 1.1.1.1
+        }
+        reverse_proxy :8096
+      '';
+    };
   };
 
   networking.firewall = {
