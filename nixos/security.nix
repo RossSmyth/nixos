4a429edd83402c6e39b1a8bf13299b3362f1482e
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-06-17 10:26:51 -0400

    nixos/security: remove pkexec

--- nixos/security.nix
+++ nixos/security.nix
@@ -2,6 +2,9 @@
   # Required for Run0 & Sway
   security.polkit.enable = true;
 
+  # Another SUID bites the dust
+  security.wrappers.pkexec.enable = false;
+  
   # Only use the good stuff
   programs.ssh.ciphers = [
     "chacha20-poly1305@openssh.com"
