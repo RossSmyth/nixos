d5ca3f4ebc07f6c44a4b2050efa71dc6e3944195
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-07-18 17:23:02 -0400

    nixos/run0: allow being targeted by nixos-rebuild

--- nixos/run0.nix
+++ nixos/run0.nix
@@ -1,5 +1,6 @@
 {
   security.sudo.enable = false;
+  system.tools.nixos-rebuild.enableRun0Elevation = true;
   security.pam.services.systemd-run0 = {
     setEnvironment = true;
     pamMount = false;
