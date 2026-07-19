6b6ad739666e3e3eea32e744ec7891d8697df5fb
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-07-18 20:57:48 -0400

    nixos/networking: add local hosts

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -7,6 +7,12 @@
       "1.1.1.1"
       "8.8.8.8"
     ];
+
+    hosts = {
+      "192.168.1.12" = "jammy.local";
+      "192.168.1.11" = "trent.local";
+      "192.168.1.10" = "desktop.local";
+    };
   };
 
   hardware.bluetooth.enable = true;
