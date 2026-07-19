ac92040bfade76b9509f4f5ab3594bc4ff6c21c1
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-07-18 22:08:48 -0400

    rebuild.sh: shellcheck

--- rebuild.sh
+++ rebuild.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-pushd "$(dirname $0)"
+pushd "$(dirname "$0")" || exit
 cmd="${1:-"dry-build"}"
 shift
 
 nixpkgs="$(npins get-path nixpkgs)"
 
 run0 nixos-rebuild "$cmd" -I nixpkgs="$nixpkgs" --log-format internal-json --show-trace --attr "$(hostname)" "$@" |& nom --json
-popd
+popd || exit
