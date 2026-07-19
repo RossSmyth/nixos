823895afd3366c437b96453c87c2d0dcd75d293a
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-07-18 21:39:21 -0400

    add remote update script

---
+++ remote.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+pushd "$(dirname "$0")" || exit
+
+host="$1"
+shift
+
+cmd="${1:-"dry-build"}"
+shift
+
+nixpkgs="$(npins get-path nixpkgs)"
+
+# Since we are building on a machine that is not the target, we
+# do not have these as substituters
+cudaFlags=("--option" "extra-substituters" "https://cache.flox.dev" "--option" "extra-trusted-public-keys" "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs=")
+
+nixos-rebuild "$cmd" -I nixpkgs="$nixpkgs" "${cudaFlags[@]}" --elevate=run0 --ask-elevate-password --target-host "rsmyth@$host.local" --attr "$host" --show-trace "$@"
+popd || exit
