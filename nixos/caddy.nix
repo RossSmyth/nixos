e6f1053224cd453706e83ced5e59a944ddab7f83
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-07-18 21:42:44 -0400

    caddy hash update

--- nixos/caddy.nix
+++ nixos/caddy.nix
@@ -9,7 +9,7 @@
       plugins = [
         "github.com/caddy-dns/cloudflare@v0.2.4"
       ];
-      hash = "sha256-8yZDrejNKsaUnUaTUFYbarWNmxafqp2z2rWo+XRsxV8=";
+      hash = "sha256-hEHgAG0F0ozHRAPuxEqLyTATBrE+pajeXDiSNwniorg=";
     };
 
     # Just log for journald
