7362e7c29f6df7ab9d20ed0e4e469523b714a6d1
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-07-26 23:54:23 -0400

    nixos/networking: use *.home

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -9,9 +9,9 @@
     ];
 
     hosts = {
-      "192.168.1.12" = [ "jammy.local" ];
-      "192.168.1.11" = [ "trent.local" ];
-      "192.168.1.10" = [ "desktop.local" ];
+      "192.168.1.12" = [ "jammy.home" ];
+      "192.168.1.11" = [ "trent.home" ];
+      "192.168.1.10" = [ "desktop.home" ];
     };
   };
 
