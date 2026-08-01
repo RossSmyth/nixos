b533b179ddb8265b29296857656ea54fa840be20
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-08-01 00:30:27 -0400

    nixos/caddy: force all logging to be to stdout, and matcher for public IPs

--- nixos/caddy.nix
+++ nixos/caddy.nix
@@ -5,47 +5,58 @@
   ...
 }:
 {
-  # Expose to the world
-  services.caddy = {
-    enable = true;
-    openFirewall = true;
-
-    package = pkgs.caddy.withPlugins {
-      # Need this for some reason.
-      plugins = [
-        "github.com/caddy-dns/cloudflare@v0.2.4"
-      ];
-      hash = "sha256-7GoH8YLCoPmPExQxoga2FHB58zQDoZVf1BBwkVi0SsQ=";
-    };
-
-    # Just log for journald
-    logFormat = ''
-      output stdout
-      format console
-    '';
+  # Force all log to be the global logging option
+  options.services.caddy.virtualHosts = lib.mkOption {
+    type = lib.types.attrsOf (
+      lib.types.submodule {
+        config.logFormat = lib.mkForce config.services.caddy.logFormat;
+      }
+    );
   };
 
-  # For local tls
-  systemd.services.caddy.path = [
-    pkgs.nss.tools
-  ];
+  config = {
+    # Expose to the world
+    services.caddy = {
+      enable = true;
+      openFirewall = true;
 
-  # oooooooo secretssss
-  systemd.services.caddy.serviceConfig.EnvironmentFile = [
-    config.age.secrets.caddy.path
-  ];
+      package = pkgs.caddy.withPlugins {
+        # Need this for some reason.
+        plugins = [
+          "github.com/caddy-dns/cloudflare@v0.2.4"
+        ];
+        hash = "sha256-7GoH8YLCoPmPExQxoga2FHB58zQDoZVf1BBwkVi0SsQ=";
+      };
 
-  users.users.caddy.extraGroups = lib.mkIf config.services.navidrome.enable [
-    config.services.navidrome.group
-  ];
+      # Just log for journald
+      logFormat = ''
+        output stdout
+        format console
+      '';
+    };
 
-  # This needs to be here or machines will get upset
-  # aka, only the machines that can decrypt can have
-  # this in their config
-  age.secrets.caddy = {
-    file = ../secrets/caddy.age;
-    mode = "400";
-    group = config.services.caddy.group;
-    owner = config.services.caddy.user;
+    # For local tls
+    systemd.services.caddy.path = [
+      pkgs.nss.tools
+    ];
+
+    # oooooooo secretssss
+    systemd.services.caddy.serviceConfig.EnvironmentFile = [
+      config.age.secrets.caddy.path
+    ];
+
+    users.users.caddy.extraGroups = lib.mkIf config.services.navidrome.enable [
+      config.services.navidrome.group
+    ];
+
+    # This needs to be here or machines will get upset
+    # aka, only the machines that can decrypt can have
+    # this in their config
+    age.secrets.caddy = {
+      file = ../secrets/caddy.age;
+      mode = "400";
+      group = config.services.caddy.group;
+      owner = config.services.caddy.user;
+    };
   };
 }
