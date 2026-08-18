866a522ed9b48db03002171320006b53b423fbdc
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-08-18 11:22:46 -0400

    nixos/slskd: improve

--- nixos/slskd.nix
+++ nixos/slskd.nix
@@ -13,7 +13,56 @@ in
   services.slskd = {
     enable = true;
     environmentFile = config.age.secrets.slskd.path;
+    settings = {
+
+      # Use a unix socket for the web server
+      web.socket = "/run/slskd/server.socket";
+      headless = false; # Set to false once setup
+
+      transfers = {
+        upload = {
+          slots = 4;
+          speed_limit = 100;
+        };
+        download = {
+          slots = 10;
+          speed_limit = 1000;
+        };
+      };
+
+      # This needs to be forwarded.
+      soulseek.listen_port = 50301;
+
+      # Lidarr-owned r/o directory
+      shares.directories = [
+        "[Music]/media/lib/music"
+      ];
+      # Downloaders-owned r/w directory
+      directories.downloads = [
+        "/media/downloads/music"
+      ];
+
+      filters.search.request = [
+        "^.{1,2}$" # Discard 1-2 char requests as they are probably spam or mistakes.
+      ];
+    };
   };
+
+  systemd.tmpfiles.settings."10-downloaders" = {
+    "/media/downloads".d = {
+      mode = "0755";
+      user = "root";
+      group = "root";
+    };
+    "/media/downloads/music".d = {
+      mode = "0755";
+      user = "root";
+      group = "root";
+    };
+  };
+
+  # tmpdir for the unix socket. Tied to the lifetime of the service.
+  systemd.services.slskd.serviceConfig.RuntimeDirectory = "slskd";
 
   age.secrets.slskd = {
     file = ../secrets/slskd.age;
@@ -21,4 +70,11 @@ in
     inherit (cfg) group;
     owner = cfg.user;
   };
+
+  services.caddy.virtualHosts."soulseek.rsmyth.net".extraConfig = ''
+    	@public not remote_ip private_ranges
+     	abort @public
+
+      reverse_proxy unix//${config.systemd.services.slskd.serviceConfig.RootDirectory}/server.socket
+  '';
 }
