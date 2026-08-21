d044359c308d8ebacfb980ecc587002d05119bbe
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-08-20 20:39:04 -0400

    nixos/default: set more aggresive oomd values

--- nixos/default.nix
+++ nixos/default.nix
@@ -17,8 +17,23 @@
   # Useful userspace oom killer
   systemd.oomd = {
     enableRootSlice = true;
+    enableSystemSlice = true;
     enableUserSlices = true;
+
+    # Logic:
+    # Generally PSI doesn't immediatly get pinned to the
+    # limit set. So this means it has spent a longer time
+    # than that going up to the limit. So 5 seconds at the
+    # limit should be long enough.
+    settings.OOM.DefaultMemoryPressureDurationSec = 5;
   };
+
+  # 80% is pretty high, I'll leave that for the root slice.
+  systemd.slices.system.sliceConfig.ManagedOOMMemoryPressureLimit = "50%";
+
+  # User limit should be relatively low since it is user-facing.
+  # I may lower it to 20% at some point.
+  systemd.slices.user.sliceConfig.ManagedOOMMemoryPressureLimit = "30%";
 
   # Use dbus-broker cause it's a bit better
   services.dbus.implementation = "broker";
