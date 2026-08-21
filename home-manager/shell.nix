fba4a0e11cb5ab41842dfc11cf88055cd242e130
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-08-20 20:40:22 -0400

    hm/btop: typo

--- home-manager/shell.nix
+++ home-manager/shell.nix
@@ -19,5 +19,6 @@
     cat = "${lib.getExe config.programs.bat.package} --paging=never";
     getLargest = "${lib.getExe config.programs.fd.package} -t file . --exec ls -s | sort -nr | head -n20";
     scratch = ''systemd-run --property=PrivateTmp=true --description "scratch shell" --user --collect --shell --working-dir "/var/tmp"'';
+    top = "${lib.getExe config.programs.btop.package}";
   };
 }
