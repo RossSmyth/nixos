f5fe25cd9aa52a0dd7589f38370c4bcc1e621411
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-08-25 22:19:40 -0400

    niri: to home-manager module

--- home-manager/niri.nix
+++ home-manager/niri.nix
@@ -5,15 +5,6 @@
   inputs,
   ...
 }:
-let
-  flake-compat = import inputs.flake-compat;
-  niri =
-    (flake-compat {
-      src = inputs.niri;
-      copySourceTreeToStore = false;
-      useBuiltinsFetchTree = true;
-    }).outputs;
-in
 {
   home.packages = with pkgs; [
     wl-clipboard-rs
@@ -45,44 +36,54 @@ in
     };
   };
 
-  programs.niri.settings.xwayland-satellite.enable = true;
-  programs.niri.settings.xwayland-satellite.path =
-    lib.getExe
-      niri.packages.${pkgs.stdenv.hostPlatform.system}.xwayland-satellite-unstable;
-
-  programs.niri = {
+  wayland.windowManager.niri = {
+    enable = true;
     package = pkgs.niri;
     settings = {
-      cursor.theme = "Hackneyed";
-      outputs = {
-        # Left desktop monitor
-        "LG Electronics LG HDR WFHD 0x0003593A" = {
-          mode = {
-            height = 1080;
-            width = 2560;
-            refresh = 74.991;
+      cursor = {
+        xcursor-theme = "Hackneyed";
+        xcursor-size = 24;
+      };
+      gestures.hot-corners.off = { };
+      input = {
+        keyboard = {
+          xkb = {
+            layout = "";
+            model = "";
+            rules = "";
+            variant = "";
           };
-          variable-refresh-rate = true;
+          repeat-delay = 600;
+          repeat-rate = 25;
+          track-layout = "global";
         };
-        # Right desktop monitor
-        "LG Electronics LG ULTRAWIDE 0x0007E9D3" = {
-          mode = {
-            height = 1080;
-            width = 2560;
-            refresh = 74.991;
-          };
-          variable-refresh-rate = true;
+        touchpad = {
+          tap = { };
+          natural-scroll = { };
+        };
+        mouse = {
+          accel-speed = 0;
+          accel-profile = "flat";
         };
       };
-      gestures.hot-corners.enable = false;
-      input.mouse = {
-        accel-speed = 0;
-        accel-profile = "flat";
+      screenshot-path = "~/Pictures/Screenshots/Screenshot from %Y-%m-%d %H-%M-%S.png";
+      layout = {
+        gaps = 16;
+        struts = {
+          left = 0;
+          right = 0;
+          top = 0;
+          bottom = 0;
+        };
+        focus-ring.width = 4;
+        border.off = { };
+        default-column-width = { };
+        center-focused-column = "never";
       };
-      spawn-at-startup = [
-        { command = [ "mako" ]; }
+      _children = [
+        { spawn-at-startup = [ "mako" ]; }
         {
-          command = [
+          spawn-at-startup = [
             (lib.getExe pkgs.swaybg)
             "-c"
             "#FFFFFF"
@@ -94,56 +95,72 @@ in
             "${./bg.jpg}"
           ];
         }
-        { command = [ "ghostty" ]; }
+        { spawn-at-startup = [ "ghostty" ]; }
+        {
+          output = {
+            _args = [ "LG Electronics LG HDR WFHD 0x0003593A" ];
+            transform = "normal";
+            mode = "2560x1080@74.991000";
+            variable-refresh-rate._props."on-demand" = false;
+          };
+        }
+        {
+          output = {
+            _args = [ "LG Electronics LG ULTRAWIDE 0x0007E9D3" ];
+            transform = "normal";
+            mode = "2560x1080@74.991000";
+            variable-refresh-rate._props."on-demand" = false;
+          };
+        }
       ];
-      binds = with config.lib.niri.actions; {
+      binds = {
         # apps
-        "Mod+Return".action.spawn = "ghostty";
-        "Mod+B".action.spawn = "firefox";
-        "Mod+D".action.spawn = [
+        "Mod+Return".spawn = "ghostty";
+        "Mod+B".spawn = "firefox";
+        "Mod+D".spawn = [
           "rofi"
           "-show"
           "drun"
         ];
-        "Mod+E".action.spawn = [
+        "Mod+E".spawn = [
           (lib.getExe pkgs.rofimoji)
         ];
         # session
-        "Mod+Alt+P".action.spawn = "poweroff";
-        "Mod+Alt+R".action.spawn = "reboot";
-        "Mod+Alt+Q".action = quit;
-        "Mod+Shift+Space".action.spawn = [
+        "Mod+Alt+P".spawn = "poweroff";
+        "Mod+Alt+R".spawn = "reboot";
+        "Mod+Alt+Q".quit = { };
+        "Mod+Shift+Space".spawn = [
           "swaylock"
           "-Ffe"
         ];
         # workspaces
-        "Mod+Space".action = toggle-overview;
-        "Mod+1".action.focus-workspace = 1;
-        "Mod+2".action.focus-workspace = 2;
-        "Mod+3".action.focus-workspace = 3;
-        "Mod+4".action.focus-workspace = 4;
-        "Mod+5".action.focus-workspace = 5;
-        "Mod+6".action.focus-workspace = 6;
-        "Mod+7".action.focus-workspace = 7;
-        "Mod+8".action.focus-workspace = 8;
-        "Mod+9".action.focus-workspace = 9;
-        "Mod+Shift+1".action.move-column-to-workspace = 1;
-        "Mod+Shift+2".action.move-column-to-workspace = 2;
-        "Mod+Shift+3".action.move-column-to-workspace = 3;
-        "Mod+Shift+4".action.move-column-to-workspace = 4;
-        "Mod+Shift+5".action.move-column-to-workspace = 5;
-        "Mod+Shift+6".action.move-column-to-workspace = 6;
-        "Mod+Shift+7".action.move-column-to-workspace = 7;
-        "Mod+Shift+8".action.move-column-to-workspace = 8;
-        "Mod+Shift+9".action.move-column-to-workspace = 9;
+        "Mod+Space".toggle-overview = { };
+        "Mod+1".focus-workspace = 1;
+        "Mod+2".focus-workspace = 2;
+        "Mod+3".focus-workspace = 3;
+        "Mod+4".focus-workspace = 4;
+        "Mod+5".focus-workspace = 5;
+        "Mod+6".focus-workspace = 6;
+        "Mod+7".focus-workspace = 7;
+        "Mod+8".focus-workspace = 8;
+        "Mod+9".focus-workspace = 9;
+        "Mod+Shift+1".move-column-to-workspace = 1;
+        "Mod+Shift+2".move-column-to-workspace = 2;
+        "Mod+Shift+3".move-column-to-workspace = 3;
+        "Mod+Shift+4".move-column-to-workspace = 4;
+        "Mod+Shift+5".move-column-to-workspace = 5;
+        "Mod+Shift+6".move-column-to-workspace = 6;
+        "Mod+Shift+7".move-column-to-workspace = 7;
+        "Mod+Shift+8".move-column-to-workspace = 8;
+        "Mod+Shift+9".move-column-to-workspace = 9;
         # sizing
-        "Mod+F".action = maximize-column;
-        "Mod+Shift+F".action = fullscreen-window;
-        "Mod+Q".action = close-window;
-        "Mod+C".action = center-column;
-        "Mod+Shift+W".action = switch-preset-column-width;
-        "Mod+Minus".action.set-column-width = "-10%";
-        "Mod+Equal".action.set-column-width = "+10%";
+        "Mod+F".maximize-column = { };
+        "Mod+Shift+F".fullscreen-window = { };
+        "Mod+Q".close-window = { };
+        "Mod+C".center-column = { };
+        "Mod+Shift+W".switch-preset-column-width = { };
+        "Mod+Minus".set-column-width = "-10%";
+        "Mod+Equal".set-column-width = "+10%";
         # move and focus
         # Try to move by-column, and focus by window. Then make
         # it work with monitor and workspaces mostly seamlessly.
@@ -151,72 +168,72 @@ in
         # I don't currently have any monitors up or down so that's
         # not really accounted for.
         ## Left
-        "Mod+WheelScrollUp".action = focus-column-left-or-last;
-        "Mod+H".action = focus-column-or-monitor-left;
-        "Mod+Shift+H".action = move-column-left;
-        "Mod+Alt+H".action = move-column-to-monitor-left;
+        "Mod+WheelScrollUp".focus-column-left-or-last = { };
+        "Mod+H".focus-column-or-monitor-left = { };
+        "Mod+Shift+H".move-column-left = { };
+        "Mod+Alt+H".move-column-to-monitor-left = { };
         ## Right
-        "Mod+WheelScrollDown".action = focus-column-right-or-first;
-        "Mod+L".action = focus-column-or-monitor-right;
-        "Mod+Shift+L".action = move-column-right;
-        "Mod+Alt+L".action = move-column-to-monitor-right;
+        "Mod+WheelScrollDown".focus-column-right-or-first = { };
+        "Mod+L".focus-column-or-monitor-right = { };
+        "Mod+Shift+L".move-column-right = { };
+        "Mod+Alt+L".move-column-to-monitor-right = { };
         ## Up
-        "Mod+K".action = focus-window-or-workspace-up;
-        "Mod+Shift+K".action = move-column-to-workspace-up;
+        "Mod+K".focus-window-or-workspace-up = { };
+        "Mod+Shift+K".move-column-to-workspace-up = { };
         ## Down
-        "Mod+J".action = focus-window-or-workspace-down;
-        "Mod+Shift+J".action = move-column-to-workspace-down;
+        "Mod+J".focus-window-or-workspace-down = { };
+        "Mod+Shift+J".move-column-to-workspace-down = { };
         # float
-        "Mod+V".action = toggle-window-floating;
-        "Mod+Shift+V".action = switch-focus-between-floating-and-tiling;
+        "Mod+V".toggle-window-floating = { };
+        "Mod+Shift+V".switch-focus-between-floating-and-tiling = { };
         # Window consume/expel to columns
-        "Mod+Ctrl+H".action = consume-or-expel-window-left;
-        "Mod+Ctrl+L".action = consume-or-expel-window-right;
+        "Mod+Ctrl+H".consume-or-expel-window-left = { };
+        "Mod+Ctrl+L".consume-or-expel-window-right = { };
         # screenshots
-        "Print".action.screenshot = { };
-        "Shift+Print".action.screenshot-screen = { };
+        "Print".screenshot = { };
+        "Shift+Print".screenshot-screen = { };
         # fn
-        "XF86AudioRaiseVolume".action.spawn = [
+        "XF86AudioRaiseVolume".spawn = [
           "wpctl"
           "set-volume"
           "@DEFAULT_AUDIO_SINK@"
           "0.1+"
         ];
-        "XF86AudioLowerVolume".action.spawn = [
+        "XF86AudioLowerVolume".spawn = [
           "wpctl"
           "set-volume"
           "@DEFAULT_AUDIO_SINK@"
           "0.1-"
         ];
-        "XF86AudioMute".action.spawn = [
+        "XF86AudioMute".spawn = [
           "wpctl"
           "set-mute"
           "@DEFAULT_AUDIO_SINK@"
           "toggle"
         ];
-        "XF86AudioPlay".action.spawn = [
+        "XF86AudioPlay".spawn = [
           "playerctl"
           "play-pause"
         ];
-        "XF86AudioNext".action.spawn = [
+        "XF86AudioNext".spawn = [
           "playerctl"
           "next"
         ];
-        "XF86AudioPrev".action.spawn = [
+        "XF86AudioPrev".spawn = [
           "playerctl"
           "previous"
         ];
-        "XF86MonBrightnessDown".action.spawn = [
+        "XF86MonBrightnessDown".spawn = [
           "brightnessctl"
           "s"
           "5%-"
         ];
-        "XF86MonBrightnessUp".action.spawn = [
+        "XF86MonBrightnessUp".spawn = [
           "brightnessctl"
           "s"
           "+5%"
         ];
-        "Mod+Shift+Slash".action = show-hotkey-overlay;
+        "Mod+Shift+Slash".show-hotkey-overlay = { };
       };
       hotkey-overlay.skip-at-startup = true;
     };
