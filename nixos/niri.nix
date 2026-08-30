9f64fcd7ce943dfeafc52c11851dba693be16749
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-08-30 00:18:02 -0400

    nixos/niri: fix with testing

--- nixos/niri.nix
+++ nixos/niri.nix
@@ -23,12 +23,8 @@
     enable = true;
     xdgOpenUsePortal = true;
     config = {
-      common.default = [ "gkt" ];
+      common.default = [ "gtk" ];
       niri = {
-        default = [
-          "gtk"
-          "gnome"
-        ];
         "org.freedesktop.impl.portal.ScreenCast" = "gnome";
         "org.freedesktop.impl.portal.Screenshot" = "gnome";
         "org.freedesktop.impl.portal.FileChooser" = "gtk";
