5451b17629b1de5e88562262dbb18d904ad129c8
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-09-05 14:12:00 -0400

    nixos/wireless: put nm networks into /var

--- nixos/wireless.nix
+++ nixos/wireless.nix
@@ -4,5 +4,9 @@
   ];
 
   networking.useDHCP = true;
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    # Put mutable state into /var
+    settings.keyfile.path = "/var/lib/NetworkManager/system-connections";
+  };
 }
