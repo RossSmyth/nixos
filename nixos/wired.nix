02f42b473ab0cdaa6228819f7606d7ec78bc3d22
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-09-04 22:44:08 -0400

    nixos/wired: disable resolved fallbacks

--- nixos/wired.nix
+++ nixos/wired.nix
@@ -17,4 +17,11 @@
       };
     };
   };
+
+  # For some reason resolved saw my DHCP-provided DNS,
+  # and then ignored it and used its builtin fallbacks.
+  # This broke local nameres I have setup.
+  #
+  # Disable so local resolution doesn't break
+  services.resolved.settings.Resolve.FallbackDNS = [ ];
 }
