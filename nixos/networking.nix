770f163a64c948ddac9ae4fdafa3c37e87e97bcd
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2026-09-05 15:15:57 -0400

    nixos/networking: use resolved

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -1,4 +1,9 @@
 { pkgs, ... }: {
+
+  # We need something to supply resolv.conf.
+  # nm isn't doing it.
+  services.resolved.enable = true;
+
   hardware.bluetooth.enable = true;
   environment.defaultPackages = with pkgs; [
     bluetui
--- nixos/wireless.nix
+++ nixos/wireless.nix
@@ -2,11 +2,6 @@
   imports = [
     ./networking.nix
   ];
-
-  # We need something to supply resolv.conf.
-  # nm isn't doing it.
-  services.resolved.enable = true;
-
   # I don't use this, and it breaks immutable etc
   networking.wireless = {
     allowAuxiliaryImperativeNetworks = false;
